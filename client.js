f3d185e61b2f73f9b5a1120635c96ea8b350b3a2
Tuan Anh Nguyen <atngu312@gmail.com>
2023-06-15 15:38:04 -0400

    experiment with movement command

--- client.js
+++ client.js
@@ -6,13 +6,18 @@ const connect = () => {
     port: 50541
   });
 
-  conn.on('data', (data) => {
-    console.log(data);
+  conn.on('data', (data) => { // 'data' event = receiving data from server
+    console.log(data); // Print out server message
   });
 
-  conn.on('connect', () => {
-    console.log('Sucessfully connected to game server');
-    conn.write('Name: TNG')
+  conn.on('connect', () => { // 'connect' event = after connection sucessfull
+    console.log('Sucessfully connected to game server'); // Print out message once connection is established
+    conn.write('Name: TNG'); // Set client's name on the server
+    // setTimeout(() => conn.write('Move: up'), 1000);
+    // setTimeout(() => conn.write('Move: right'), 2000);
+    // setTimeout(() => conn.write('Move: up'), 3000);
+    // setTimeout(() => conn.write('Move: left'), 4000);
+    // setInterval(() => conn.write('Move: up'), 50);
   });
 
 
