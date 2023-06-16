1ca348a69c2e1aedd63c81cadcc664315af6094d
Tuan Anh Nguyen <atngu312@gmail.com>
2023-06-15 23:22:22 -0400

    update comment and add back commented-out codes

--- input.js
+++ input.js
@@ -3,11 +3,11 @@ const { pretyped } = require('./constants');
 let connection;
 
 const handleUserInput = (input) => {
-  if (input === '\u0003') {
+  if (input === '\u0003') { // Ctrl + c to exit the app
     process.exit();
   }
 
-  if (pretyped[input]) {
+  if (pretyped[input]) { // Send message to server if the input is included in the pretyped object
     connection.write(pretyped[input]);
   }
 };
