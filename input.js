d84c81510e8cd881daa5b0a6d6cf593a053a20dc
Tuan Anh Nguyen <atngu312@gmail.com>
2023-06-15 21:53:21 -0400

    pass connection into setupInput as a parameter

--- input.js
+++ input.js
@@ -1,10 +1,25 @@
+let connection;
+
 const handleUserInput = (input) => {
   if (input === '\u0003') {
     process.exit();
   }
+  if (input === 'w') {
+    connection.write('Move: up');
+  }
+  if (input === 'a') {
+    connection.write('Move: left');
+  }
+  if (input === 's') {
+    connection.write('Move: down');
+  }
+  if (input === 'd') {
+    connection.write('Move: right');
+  }
 };
 
-const setupInput = () => {
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
